76f195e6233677af3c6abfa48fff56a7b88c61f6
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2023-04-26 19:07:32 +0300

    Update MainActivity.kt

--- app/src/main/java/com/atilsamancioglu/classesandfunctions/MainActivity.kt
+++ app/src/main/java/com/atilsamancioglu/classesandfunctions/MainActivity.kt
@@ -3,128 +3,124 @@ package com.atilsamancioglu.classesandfunctions
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
-
 class MainActivity : AppCompatActivity() {
 
-    lateinit var nameText : EditText
-    lateinit var ageText : EditText
-    lateinit var jobText : EditText
-    lateinit var resultText : TextView
-    private var name = ""
+    private lateinit var myTextView : TextView
+    private lateinit var myButton : Button
+    private lateinit var nameText : EditText
+    private lateinit var ageText : EditText
+    private lateinit var jobText : EditText
+    var name = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        myTextView = findViewById(R.id.textView)
+        myButton = findViewById(R.id.button)
         nameText = findViewById(R.id.nameText)
         ageText = findViewById(R.id.ageText)
         jobText = findViewById(R.id.jobText)
-        resultText = findViewById(R.id.resultText)
-        test()
-
-        //Void - Unit
-        //mySum(10,20)
-
-        //Return
-
-        val result = myMultiply(10,20)
-        //textView.text = "Result: ${result}"
 
 
         /*
-        button.setOnClickListener {
-            println("clicked")
+        myButton.setOnClickListener {
+            myTextView.text = "button clicked 2"
         }
 
          */
 
+        // 1) code block, reusable 2) input 3) output - return
+
+        //println("hello kotlin")
+        test()
+        mySum(10,15)
+
+        val sumResult =  mySum(40,35)
+        val result = myMultiply(10,20)
+
+        println(sumResult)
+        println(result)
+
+
         //Class
 
-        //Object & Instance
+        val homer = Simpson("Homer", 50, "Nuclear")
 
-        val homer = Simpson("Homer Simpson",50,"Nuclear")
+        homer.setHeight(50)
 
-        homer.hairColor = "Yellow"
-
-        //homer.age = 60
-        //homer.job = "Nuc"
-        //homer.name = "Homer"
-
-        println(homer.hairColor)
 
         //Nullability
-        // Nullable (?) && Non-null
-        // !! vs ?
 
         var myString : String? = null
+        myString = "test"
+        println(myString)
+
         var myAge : Int? = null
+        //myAge = 50
+        // !! ?
 
+        //1) !!
+        //println(myAge!! * 10)
 
-        //1)Null safety
+        //2) safe call
 
-        if (myAge != null) {
-            println(myAge * 10)
+        println(myAge?.minus(10))
+
+        //3)
+
+        if(myAge != null) {
+            println(myAge.minus(10))
         } else {
-            println("myAge null")
+            println("myAge is null")
         }
 
-        //2)safe call
 
-        println(myAge?.compareTo(2))
+        //4) elvis operator
 
-        //3)elvis
+        println(myAge?.minus(10) ?: -10)
 
-        val myResult = myAge?.compareTo(2) ?: -100
-        println(myResult)
-
-
+        //5)
+        myAge?.let {
+            println(it * 10)
+        }
 
     }
 
+    fun test() {
+        //println("test function")
 
-    fun test(){
-        println("test function")
     }
 
-    //Input & Return
+    fun mySum(a:Int, b:Int) {
 
-    fun mySum(a: Int,b: Int ) {
-        //textView.text = "Result: ${a+b}"
     }
 
-    fun myMultiply(x:Int, y:Int) : Int {
-        return x * y
+    fun myMultiply(x: Int, y: Int) : Int {
+
+        //println(x*y)
+        val result = x * y
+        myTextView.text = "Result: ${result}"
+
+        return result
     }
 
-    fun makeSimpson(view : View) {
-
+    fun buttonClicked(view : View) {
         name = nameText.text.toString()
-        var age = ageText.text.toString().toIntOrNull()
-        if (age == null) {
-            age = 0
-        }
+        val age = ageText.text.toString().toIntOrNull()
         val job = jobText.text.toString()
-
-        val simpson = Simpson(name,age,job)
-
-        resultText.text = "Name : ${simpson.name}, Age: ${simpson.age}, Job: ${simpson.job}"
-
-
-
-
+        if (age != null) {
+            val simpson = Simpson(name, age, job)
+            myTextView.text = "Name: ${simpson.name} Age: ${simpson.age} Job: ${simpson.job}"
+        } else {
+            myTextView.text = "Enter your age!"
+        }
 
     }
-
-    fun scopeExample(view : View) {
-        //Scope
-
-        println(name)
-
-
-    }
-
-
 
 }
